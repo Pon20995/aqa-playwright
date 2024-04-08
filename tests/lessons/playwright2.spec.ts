3704e7ef06b1735f6311c06673ec7a018c84e76c
Pon20995 <ozera123456789@gmail.com>
2024-04-08 23:15:21 +0100

    added_files_with_playwright_lesson2

---
+++ tests/lessons/playwright2.spec.ts
@@ -0,0 +1,46 @@
+import { test, expect } from "@playwright/test";
+import { LoginPage } from "../../pageObjects/loginPage";
+import { Header } from "../../components/header";
+
+test.beforeEach(async ({ page }) => {
+  //   const loginpage = new LoginPage(page);
+  await page.goto("/");
+});
+
+test("Test with valid data", async ({ page }) => {
+  const loginpage = new LoginPage(page);
+  const buttonLogin = await loginpage.buttonLogin();
+
+  await loginpage.loginWithDefaultParams();
+  await expect(buttonLogin).toBeVisible();
+  await buttonLogin.click();
+
+  //   await page.getByRole("button", { name: "Sign In" }).click();
+  //   await page.getByLabel("Email").click();
+  //   await page.getByLabel("Email").fill("ozera123456789@gmail.com");
+  //   await page.getByLabel("Password").click();
+  //   await page.getByLabel("Password").fill("R4gnZxg.J7U7EX");
+  //   await page.getByRole("button", { name: "Login" }).click();
+
+  //   await page.getByRole("button", { name: "Add car" }).click();
+  //   await page.getByLabel("Brand").selectOption("3: 4");
+  //   await page.getByLabel("Model").selectOption("10: 18");
+  //   await page.getByLabel("Mileage").click();
+  //   await page.getByLabel("Mileage").fill("101010");
+  //   await page.getByRole("button", { name: "Add" }).click();
+
+  await expect(page.getByText("Porsche Panamera")).toBeVisible();
+  await expect(page.locator("app-car")).toContainText(
+    "Update mileage • 08.04.2024"
+  );
+});
+
+test("Login test with dynamic data", async ({ page }) => {
+  const loginpage = new LoginPage(page);
+  const header = new Header(page);
+  await header.logoVisisble();
+  await loginpage.loginWithDynamicData(
+    "ozera123456789@gmail.com",
+    "R4gnZxg.J7U7EX"
+  );
+});

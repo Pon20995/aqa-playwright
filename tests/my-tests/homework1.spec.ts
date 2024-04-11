8b64b91379cb0bfb1f8c7471e464054f400cece9
Pon20995 <ozera123456789@gmail.com>
2024-04-11 23:41:24 +0100

    added_files_with_playwright_lesson3

--- tests/my-tests/homework1.spec.ts
+++ tests/my-tests/homework1.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from "@playwright/test";
 
-const loginName = "ozera123456789@gmail.com";
-const loginPass = "R4gnZxg.J7U7EX";
+const loginName = process.env.LOGIN_USERNAME;
+const loginPass = process.env.LOGIN_PASS;
 
 test.beforeEach(async ({ page }) => {
   // Перевикористали конструкцію в конфіг файлі, з заміною в test browser на page
@@ -20,32 +20,38 @@ test.beforeEach(async ({ page }) => {
   await expect(page.locator("div.panel-page_heading h1")).toHaveText("Garage");
 });
 
-test.describe("Adding and Deleting check", () => {
-  test.describe.configure({ mode: "serial" });
+test.describe(
+  "Adding and Deleting check",
+  {
+    tag: ["@addCar"],
+  },
+  () => {
+    test.describe.configure({ mode: "serial" });
 
-  test("Add car", async ({ page }) => {
-    await page.locator('button:has-text("Add car")').click();
-    await page.selectOption("select#addCarBrand", "Fiat");
-    await page.selectOption("select#addCarModel", "Ducato");
-    await page.fill("input[name='mileage']", "300000");
-    await page.locator("div.justify-content-end button.btn-primary").click();
-    await expect(
-      page.locator("div").filter({ hasText: "Car added" }).nth(3)
-    ).toHaveText("Car added");
-  });
+    test("Add car", async ({ page }) => {
+      await page.locator('button:has-text("Add car")').click();
+      await page.selectOption("select#addCarBrand", "Fiat");
+      await page.selectOption("select#addCarModel", "Ducato");
+      await page.fill("input[name='mileage']", "300000");
+      await page.locator("div.justify-content-end button.btn-primary").click();
+      await expect(
+        page.locator("div").filter({ hasText: "Car added" }).nth(3)
+      ).toHaveText("Car added");
+    });
 
-  test("Delete a car", async ({ page }) => {
-    if (await page.locator("span.icon-edit").first().isVisible()) {
-      await page.locator("button.btn-edit").first().click();
-      await page.locator('button:has-text("Remove car")').click();
-      await page.locator("button.btn-danger").click();
-      await expect(page.locator("p.panel-empty_message")).toHaveText(
-        "You don’t have any cars in your garage"
-      );
-    } else {
-      await expect(page.locator("p.panel-empty_message")).toHaveText(
-        "You don’t have any cars in your garage"
-      );
-    }
-  });
-});
+    test("Delete a car", async ({ page }) => {
+      if (await page.locator("span.icon-edit").first().isVisible()) {
+        await page.locator("button.btn-edit").first().click();
+        await page.locator('button:has-text("Remove car")').click();
+        await page.locator("button.btn-danger").click();
+        await expect(page.locator("p.panel-empty_message")).toHaveText(
+          "You don’t have any cars in your garage"
+        );
+      } else {
+        await expect(page.locator("p.panel-empty_message")).toHaveText(
+          "You don’t have any cars in your garage"
+        );
+      }
+    });
+  }
+);

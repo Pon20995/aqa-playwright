06b9dbe7341b6601fd92123b685bf7c5bf365b41
Pon20995 <ozera123456789@gmail.com>
2024-04-24 01:49:15 +0100

    added_files_with_playwright_lesson4

--- playwright.config.ts
+++ playwright.config.ts
@@ -12,7 +12,7 @@ configDotenv({ path: `env/.env.${process.env.ENV}` });
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  testDir: "./tests/lessons",
+  testDir: "./tests/my-tests",
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
@@ -22,7 +22,16 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [["list"], ["html"]],
+  reporter: [
+    ["list"],
+    ["html", { open: "never" }],
+    [
+      "@testomatio/reporter/lib/adapter/playwright.js",
+      {
+        apiKey: process.env.TESTOMATIO,
+      },
+    ],
+  ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
@@ -34,7 +43,9 @@ export default defineConfig({
     },
     ignoreHTTPSErrors: true,
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: "on-first-retry",
+    trace: "retain-on-first-failure",
+    screenshot: "only-on-failure",
+    video: "retain-on-failure",
   },
 
   /* Configure projects for major browsers */
